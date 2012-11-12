75ae6effc51a239b38a67dfdde6508e5d7769ea8
Martin Zitnik <zito2004@seznam.cz>
2012-11-12 14:07:29 +0100

    Added is active attribute to Card

--- Pexeso-ejb/src/main/java/cz/muni/fi/hackaton/Pexeso/model/Card.java
+++ Pexeso-ejb/src/main/java/cz/muni/fi/hackaton/Pexeso/model/Card.java
@@ -14,6 +14,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Transient;
 
 /**
  *
@@ -27,6 +28,8 @@ public class Card implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     
+    @Transient
+    private Boolean isActive = true;
     private String name;
     private URL image;
     
